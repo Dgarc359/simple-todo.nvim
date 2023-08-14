09a767992178353c03b3e362494a7358f9af13f6
David Garcia <87497958+Dgarc359@users.noreply.github.com>
2023-08-14 17:39:49 -0400

    modify util

--- lua/simple-nvim-todo/util.lua
+++ lua/simple-nvim-todo/util.lua
@@ -2,30 +2,30 @@ local M = {}
 function M.pretty_print(value, indent, already_printed)
   indent = indent or ""
   already_printed = already_printed or {}
-  local type = type(value)
-  if type == "table" then
+  local t = type(value)
+  if t == "table" then
     if already_printed[value] then
       io.write("...a repeat of a previous table...")
     else
       already_printed[value] = true
       io.write("{\n")
       local subindent = indent .. "  "
-      for k,v in pairs(type) do
+      for k,v in pairs(value) do
         io.write(subindent)
         if type(k) == "string" and k:match("^[_a-zA-Z][_a-zA-Z0-9]*$") then
           io.write(k)
         else
           io.write("[")
-          pretty_print(k)
+          M.pretty_print(k)
           io.write("]")
         end
         io.write(" = ")
-         pretty_print(v)
+         M.pretty_print(v)
         io.write(",\n")
       end
       io.write(indent.."}")
     end
-  elseif type == "string" then
+  elseif t == "string" then
     -- print strings with quotes and escapes
     io.write(("%q"):format(value))
   else
