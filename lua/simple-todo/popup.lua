5c0ef3a18023310486940ccffca11b980e908b66
David Garcia <87497958+Dgarc359@users.noreply.github.com>
2023-08-22 20:56:40 -0400

    read from table

--- lua/simple-todo/popup.lua
+++ lua/simple-todo/popup.lua
@@ -1,15 +1,24 @@
 local popup = require("plenary.popup")
+local read = require("simple-todo.read")
 
 local M = { }
 
-Todo_win_id = nil
-Todo_win_bufh = nil
+Todo_win_id = nil Todo_win_bufh = nil
 
 local function create_ui()
 
     local bufnr = vim.api.nvim_create_buf(false, true)
+    local width = 60
+    local height = 10
 
-    local Todo_win_id, win= popup.create(bufnr, {
+    local Todo_win_id, win= popup.create(
+--        { "item 1", "item 2"},
+        bufnr, 
+    {
+        title = "simple-todo",
+        columns = math.floor((vim.o.columns - width) / 2),
+        minWidth = width,
+        minHeight = height,
         borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
     })
 
@@ -20,7 +29,6 @@ local function create_ui()
 end
 
 local function close_menu()
-
     vim.api.nvim_win_close(Todo_win_id, true)
     Todo_win_id = nil
     Todo_win_bufh = nil
@@ -39,6 +47,11 @@ function M.toggle_ui()
 
     vim.api.nvim_buf_set_name(Todo_win_bufh, "simple-todo-menu")
     vim.api.nvim_buf_set_option(Todo_win_bufh, "bufhidden", "delete")
+    local items_to_display = read.todos()
+
+    print("items length:" .. #items_to_display)
+    local fake = { "item 1", "item 2"}
+    vim.api.nvim_buf_set_lines(Todo_win_bufh, 0, #fake, false, fake)
     vim.api.nvim_buf_set_keymap(
         Todo_win_bufh,
         "n",
--- lua/simple-todo/read.lua
+++ lua/simple-todo/read.lua
@@ -8,21 +8,12 @@ function M.todos()
     local db = sqlite:open("todo.db")
 
     local items = db:select("todo_list", {where = { completed = 'No' }})
-    -- local items = sqlite:with_open("todo.db", function(db)
-    --   return db:eval("SELECT * FROM todo_list WHERE completed == 'No';")
-    -- end)
-    -- util.pretty_print(items, "")
     local mapped_items = {}
     table.foreach(items, function(k, v)
-       table.insert(mapped_items, v.description)
+       table.insert(mapped_items, string.format("%s", v.description))
     end)
 
-
-   -- local win_id = popup.create({ "menu item 1"}, {})
-   -- local win_id = popup.create(mapped_items, {
-   --      borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
-   -- })
-   --
+    return mapped_items
 end
 
 return M
